590de5dea1980c367ff37aef4849b2341e16af3e
OussamaBelkadi <OussamaYC@hotmail.com>
2024-07-24 16:18:56 +0100

    how can i display the loading page when the page is loaded

---
+++ snippets/src/app/snippet/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function SnippetShowLoadingPage() {
+    return(
+        <div className="flex justify-center items-center">
+            <h1>Loading ...</h1>
+        </div>
+    )
+}
--- snippets/src/app/snippet/[id]/page.tsx
+++ snippets/src/app/snippet/[id]/page.tsx
@@ -8,7 +8,7 @@ interface SnippetParam{
 }
 
 export default async function Snippet(props: SnippetParam) {
-   
+   await new Promise((r) => setTimeout(r,2000))
     const snippet = await db.snippet.findFirst({
         where: { id : parseInt(props.params.id) }
     });
