611cf567ec4c2e9705bc07f6b8ae63033fc07230
OussamaBelkadi <OussamaYC@hotmail.com>
2024-07-29 15:48:04 +0100

    in first steep, configuration suthentication OAuth

---
+++ discuss/src/auth.tsx
@@ -0,0 +1,28 @@
+import NextAuth  from "next-auth";
+import GitHub from "@auth/core/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from '@/db';
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+
+  throw new Error("Missing github oauth credentials");
+    
+}
+// Configuration object 
+NextAuth({
+    adapter: PrismaAdapter(db),
+    providers:[
+        GitHub({
+            clientId: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET
+        })
+    ],
+    callbacks:{
+        async session(){
+            
+        }
+    }
+})
--- discuss/tailwind.config.ts
+++ discuss/tailwind.config.ts
@@ -1,10 +1,17 @@
 import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/react'
 
 const config: Config = {
   content: [
+     //  this entire section right here of content is telling tailwind where to go to find all the different
+    // class names that we are using inside of our project.
+    // So you'll notice that we are telling tailwind to go and look inside of our app directory and go and
+    // find all the different JavaScript, TTS, JSX, TSX files inside there.
+    // Same thing with the components directory.
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -15,6 +22,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
